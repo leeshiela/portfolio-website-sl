eab00e670b09cd84343adb6573f74c8dc09e2b52
Shiela <leeshiela@gmail.com>
2019-04-06 11:10:39 -0400

    Station Rotation Click

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //Sticky mySidenav (Veritcal Navigation Bar)
 // When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 // Get the navbar
 var mySidenav = document.getElementById("mySidenav");
@@ -49,52 +49,76 @@ document.getElementById("defaultOpen").click();
 //-------------------------------------------------------------
 //Station Tool page
 function showInput() {
-//Station 1 name & members
-  document.getElementById("station-1-name-header").innerHTML = 
-  document.getElementById("station-1-name").value
-  document.getElementById("station-1-materials-output").innerHTML = 
-  document.getElementById("station-1-materials").value
-  document.getElementById("station-1-members-output").innerHTML = 
-  document.getElementById("station-1-members").value
-//Station 2 name & members
-  document.getElementById("station-2-name-header").innerHTML = 
-  document.getElementById("station-2-name").value
-  document.getElementById("station-2-materials-output").innerHTML = 
-  document.getElementById("station-2-materials").value  
-  document.getElementById("station-2-members-output").innerHTML = 
-  document.getElementById("station-2-members").value
-//Station 3 name & members
-  document.getElementById("station-3-name-header").innerHTML = 
-  document.getElementById("station-3-name").value
-  document.getElementById("station-3-materials-output").innerHTML = 
-  document.getElementById("station-3-materials").value
-  document.getElementById("station-3-members-output").innerHTML = 
-  document.getElementById("station-3-members").value
-  }
+  //Station 1 name & members
+  document.getElementById("station-1-name-header").innerHTML =
+    document.getElementById("station-1-name").value
+  document.getElementById("station-1-materials-output").innerHTML =
+    document.getElementById("station-1-materials").value
+  document.getElementById("station-1-members-output").innerHTML =
+    document.getElementById("station-1-members").value
+  //Station 2 name & members
+  document.getElementById("station-2-name-header").innerHTML =
+    document.getElementById("station-2-name").value
+  document.getElementById("station-2-materials-output").innerHTML =
+    document.getElementById("station-2-materials").value
+  document.getElementById("station-2-members-output").innerHTML =
+    document.getElementById("station-2-members").value
+  //Station 3 name & members
+  document.getElementById("station-3-name-header").innerHTML =
+    document.getElementById("station-3-name").value
+  document.getElementById("station-3-materials-output").innerHTML =
+    document.getElementById("station-3-materials").value
+  document.getElementById("station-3-members-output").innerHTML =
+    document.getElementById("station-3-members").value
+}
 
 
 //Iterating by number of clicks by adding one to the ID to switch places
-function showOutputOne() {
-    var rotation=1;    
-    //Station 1 name & members
-      document.getElementById("station-1-name-header").innerHTML = 
-      document.getElementById("station-1-name").value
-      document.getElementById("station-1-materials-output").innerHTML = 
-      document.getElementById("station-1-materials").value
-      document.getElementById("station-" + (rotation%3 + 1) + "-members-output").innerHTML = 
-      document.getElementById("station-1-members").value
-    //Station 2 name & members
-      document.getElementById("station-2-name-header").innerHTML = 
-      document.getElementById("station-2-name").value
-      document.getElementById("station-2-materials-output").innerHTML = 
-      document.getElementById("station-2-materials").value  
-      document.getElementById("station-" + (rotation%3 + 2) + "-members-output").innerHTML = 
-      document.getElementById("station-2-members").value
-    //Station 3 name & members
-      document.getElementById("station-3-name-header").innerHTML = 
-      document.getElementById("station-3-name").value
-      document.getElementById("station-3-materials-output").innerHTML = 
-      document.getElementById("station-3-materials").value
-      document.getElementById("station-" + (rotation%3) + "-members-output").innerHTML = 
-      document.getElementById("station-3-members").value
-      }
+/*function showOutputOne() {
+  var rotation = 1;
+  //Station 1 name & members
+  document.getElementById("station-1-name-header").innerHTML =
+    document.getElementById("station-1-name").value
+  document.getElementById("station-1-materials-output").innerHTML =
+    document.getElementById("station-1-materials").value
+  document.getElementById("station-" + (rotation % 3 + 1) + "-members-output").innerHTML =
+    document.getElementById("station-1-members").value
+  //Station 2 name & members
+  document.getElementById("station-2-name-header").innerHTML =
+    document.getElementById("station-2-name").value
+  document.getElementById("station-2-materials-output").innerHTML =
+    document.getElementById("station-2-materials").value
+  document.getElementById("station-" + (rotation % 3 + 2) + "-members-output").innerHTML =
+    document.getElementById("station-2-members").value
+  //Station 3 name & members
+  document.getElementById("station-3-name-header").innerHTML =
+    document.getElementById("station-3-name").value
+  document.getElementById("station-3-materials-output").innerHTML =
+    document.getElementById("station-3-materials").value
+  document.getElementById("station-" + (rotation % 3) + "-members-output").innerHTML =
+    document.getElementById("station-3-members").value
+}
+*/
+
+function showOutput() {
+//give variables names to call out the containers that hold our students' names
+  var station1 = document.getElementById("station-1-members-output");
+  var station2 = document.getElementById("station-2-members-output");
+  var station3 = document.getElementById("station-3-members-output");
+
+
+//get the students' name as a string. These strings change as they are the strings as they are before or after the rotate button is clicked.
+
+var station1members = station1.innerHTML;
+var station2members = station2.innerHTML;
+var station3members = station3.innerHTML;
+
+//Switch around the student names. Here we take a look at our containers. In it we replace the strings with new strings, thus, "rottating the members".
+
+//station 1 container will now have members previously from station 2.
+station1.innerHTML = station3members;
+//station 2 container will now have members previously from station 3.
+station2.innerHTML = station1members;
+//station 3 container will now have members previously from station 1.
+station3.innerHTML = station2members;
+}
